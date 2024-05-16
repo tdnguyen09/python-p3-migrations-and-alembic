e9a6a5b3efe3cd4e359a68fe3453b3c839efa94b
Thuy Duong Nguyen <thuyduong.nguyen.091096@gmail.com>
2024-05-16 13:53:59 +1000

    complete

---
+++ lib/migrations/versions/cab9eecfcd2e_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: cab9eecfcd2e
+Revises: 
+Create Date: 2024-05-16 13:31:19.217578
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'cab9eecfcd2e'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
